0c441d213d882aba9753b7299b1df82995ab26ac
Margarita Kosareva <margaritakosareva1@gmail.com>
2020-01-21 19:23:39 +0300

    main.js edited

---
+++ 1.1-variables/main.js
@@ -0,0 +1,28 @@
+// Задача 1
+let algebra;
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let average = (algebra + geography + physics) / 3;
+console.log(average);
+
+// Задача 2
+let myName = 'Маргарита';
+let message = `Привет, мир! Меня зовут ${myName}.`;
+console.log(message);
+
+// Задача 3
+let x;
+let y;
+let z;
+
+x = 2;
+y = 22;
+z = 0;
+
+let result = x * y + 5 * z + x - 1 ;
+console.log(result);
