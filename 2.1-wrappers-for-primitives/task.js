57710a6d2e0e8d8d3d76b51b8f809fc29c837c09
M0r0shka <52579668+M0r0shka@users.noreply.github.com>
2020-02-08 14:09:40 +0300

    Update task.js

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -10,9 +10,34 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    const today = new Date();
+    const contributeDate = new Date(date);
+    let differenceDate = Math.abs(contributeDate.getTime() - today.getTime());
+    date = Math.floor(differenceDate / (1000 * 3600 * 24 * 30.4166));
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    let arr = {
+      percent: percent, 
+      contribution: contribution, 
+      amount: amount, 
+      date: date,
+    }
+
+    for (let prop in arr) {
+      let value = arr[prop];
+      //console.log(value);
+      let parsed = parseInt(value, 10);
+      arr[prop] = parsed;
+      if (isNaN(parsed)) { 
+        console.log(`Параметр \"${prop}\" содержит неправильное значеие ${value}`); 
+      } 
+    }
+
+    let p = arr.percent / (100 * 12);
+    let summ = arr.amount - arr.contribution;
+    let monthPay = summ * (p + p / (((1 + p)**date) - 1));
+    let total = monthPay * date;
+    let totalAmount = total.toFixed(2);
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +48,11 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+  let greeting;
+  if (name == !!0 || name == undefined) {
+    greeting = `Привет мир! Меня зовут Аноним.`;
+    } else {
+      greeting = `Привет мир! Меня зовут ${name}`;
+    }
+  return greeting;
 }
